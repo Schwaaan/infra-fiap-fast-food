8af10323c92a6c97c1a59b72751d0bfda5261849
Jair Schwan <jair.schwan9@gmail.com>
2024-03-19 22:11:09 -0300

    add new form for push  in docker hub

--- main.tf
+++ main.tf
@@ -2,10 +2,13 @@ provider "aws" {
   region = "sua-região"
 }
 
-resource "aws_instance" "example" {
-  ami           = "sua-AMI"  
-  instance_type = "t2.micro" 
-  tags = {
-    Name = "exemplo-ec2"
+resource "aws_eks_cluster" "cluster01" {
+  name     = "cluster01"
+
+  vpc_config {
+    subnet_ids = ["subnet-01d613d36b077395c" 
+                  "subnet-0a3dfb20aaa7283dc","subnet-04dd13bd928c5cc4b",
+                  "subnet-00124a3cd3750b558", "subnet-0bcf8a6496c490660"]
+    security_group_ids = ["sg-0c24cb30f05d8a03b"]
   }
 }
